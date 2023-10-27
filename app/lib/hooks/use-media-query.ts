26a898dae45179202939cdd404d1f1dd7fd2ed3f
peymanath <naderidefault@gmail.com>
2023-10-27 12:44:28 +0330

    update command

--- app/lib/hooks/use-media-query.ts
+++ app/lib/hooks/use-media-query.ts
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import type { MediaQueryReturn } from '@/types/hooks.types';
+import { MediaQueryReturn } from '@/app/types';
 
 export const useMediaQuery = (): MediaQueryReturn => {
   // States;
